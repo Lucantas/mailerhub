5d8c293496f8d0fd9ab54204951c086285357d4d
Lucas <lucas.lucantas38@gmail.com>
2019-01-07 23:57:16 -0200

    updated src directory

--- internal/pkg/mailerdb/models.go
+++ internal/pkg/mailerdb/models.go
@@ -4,12 +4,19 @@ package mailerdb
 type client struct {
 	SenderEmail string
 	Password    string
+	Mailers     []mailer
 }
 
 // Mail type with the basic information to be send by clients
 type mail struct {
 	Subject string
 	Body    string
+}
+
+type mailer struct {
+	destinataries []string
+	provider      string
+	senderAddress string
 }
 
 type conn struct {
